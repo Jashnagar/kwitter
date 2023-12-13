6b72a7b76c7add7b8bdee6dc11642acdf670deb0
Jashnagar <126826939+Jashnagar@users.noreply.github.com>
2023-12-13 16:57:12 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,9 +1,9 @@
 function addUser()
 {
     
-    user_name = document.getElementById("user_name").vale;
+    user_name = document.getElementById("user_name").value;
     
-    localStorage.setItem("user_name", usr_name);
+    localStorage.setItem("user_name", user_name);
     
     window.location = "Kwitter_room.html";
 }
